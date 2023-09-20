edeb62ac45e91b575ef6debe7d937cebaebe9d6c
Foxify52 <93794624+Foxify52@users.noreply.github.com>
2023-09-19 22:02:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Loop-O-Matic
+This awful piece of software is the loop-o-matic. It's a simple program that allows you to create a meh quality extention of any given song. This was my sad attempt at making a stripped down clone of the incredible [Eternal Jukebox](https://jukebox.davi.gq/jukebox_index.html) that didn't rely on spotify or youtube.
+
+## Usage
+Run `pip install -r requirements.txt`.
+
+Run `main.py`. Make sure you fill out the settings. They have comments telling you what goes where.
+
+## Known Issues
+The jumps are not perfect. They are a little bit off.
+It also has issues with the beats being played twice. This is because the beat detection within librosa is a little bit off which throws the whole algorithm off.
+Nothing I know what to do about either one of these issues so if you do, feel free to leave a PR.
+
+## Licensing
+   Copyright 2023 Foxify52
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
---
+++ main.py
@@ -0,0 +1,83 @@
+import random, librosa
+import numpy as np
+import networkx as nx
+import soundfile as sf
+from scipy.spatial.distance import cdist
+
+def decompose_beats(y, sr):
+    onset_env = librosa.onset.onset_strength(y=np.mean(y, axis=0), sr=sr)
+    tempo, beat_frames = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
+    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+    return beat_times
+
+def analyze_beats(y, sr, beat_times):
+    S = librosa.feature.melspectrogram(y=np.mean(y, axis=0), sr=sr, n_mels=128)
+    log_S = librosa.amplitude_to_db(S, ref=np.max)
+    beat_features = []
+    for beat_time in beat_times:
+        beat_sample = librosa.time_to_frames(beat_time, sr=sr)
+        beat_feature = log_S[:, beat_sample]
+        beat_histogram = np.histogram(beat_feature, bins=10)[0]
+        beat_features.append(np.concatenate([beat_feature, beat_histogram]))
+    return np.array(beat_features)
+
+def create_song_graph(beat_features):
+    distance_matrix = cdist(beat_features, beat_features, 'cosine')
+    graph = nx.from_numpy_array(distance_matrix)
+    return graph
+
+def compute_song(y, sr, graph, beat_times, jumps, beat_match_length, jump_interval):
+    song = []
+    current_beat = 0
+    potential_jumps = 0
+    jump_count = 0
+    while jump_count <= jumps:
+        if current_beat >= len(beat_times) - 1:
+            current_beat = 0
+        start_time = int(beat_times[current_beat] * sr)
+        end_time = int(beat_times[current_beat + 1] * sr)
+        if end_time <= len(y[0]):
+            song.append(y[:, start_time:end_time])
+        else:
+            song.append(y[:, start_time:])
+            current_beat = 0
+        print(f"Jump count: {jump_count}, Current beat: {current_beat}, Passed potential jumps: {potential_jumps}")
+        similar_beats = [node for node in graph.neighbors(current_beat)]
+        if len(similar_beats) > 0:
+            potential_jumps += 1
+            if random.random() < 0.2 and potential_jumps >= jump_interval:
+                similar_beats = [beat for beat in similar_beats if len(set(graph[current_beat]) & set(graph[beat])) >= beat_match_length and beat != current_beat]
+                if len(similar_beats) > 0:
+                    current_beat = min(similar_beats, key=lambda x: np.linalg.norm(beat_features[current_beat] - beat_features[x]))
+                    potential_jumps = 0
+                    jump_count += 1
+                else:
+                    current_beat += 1
+            else:
+                current_beat += 1
+        else:
+            current_beat += 1
+    while current_beat < len(beat_times) - 1:
+        start_time = int(beat_times[current_beat] * sr)
+        end_time = int(beat_times[current_beat + 1] * sr)
+        if end_time <= len(y[0]):
+            song.append(y[:, start_time:end_time])
+        else:
+            song.append(y[:, start_time:])
+            current_beat = 0
+        current_beat += 1
+    final_song = np.concatenate(song, axis=1)
+    return final_song
+
+audio_file = "" # Set your song's file path here. A valid example is C:\\users\\music\\ballin.mp3
+base_sr = librosa.get_samplerate(audio_file)
+y, sr = librosa.load(audio_file, sr=base_sr, mono=False)
+beat_times = decompose_beats(y, sr)
+beat_features = analyze_beats(y, sr, beat_times)
+graph = create_song_graph(beat_features)
+song_array = compute_song(y, sr, graph, beat_times, 10, 45, 20).T 
+# Don't mess with first 4 values of the compute song function. Last 3 can be configured and control the following:
+# The number of times the program can jump around the song,.
+# The number of concecutive beats that must match before a jump can take place.
+# The number of times jumps are guaranteed to be skipped before the next jump is allowed to take place.
+sf.write('output.mp3', song_array, sr)
